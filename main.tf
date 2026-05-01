1cee59b05abb208f7371fbc73983b30ab77486b7
setthapong4u <setthapongu4@gmail.com>
2026-05-01 14:10:41 +0000

    change instance_type

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_security_group" "ec2" {
 
 resource "aws_instance" "demo" {
   ami                         = data.aws_ami.amazon_linux_2023.id
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ec2.id]
   key_name                    = var.key_name
